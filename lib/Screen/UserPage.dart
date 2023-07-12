9223bf2b680048fc77d2cdf96de0c0d7ff02e3eb
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-07-12 23:51:29 +0530

    .

---
+++ lib/Screen/UserPage.dart
@@ -0,0 +1,159 @@
+// import "package:flutter/material.dart";
+// import 'package:test/UserTextField.dart';
+
+// class UserPage extends StatelessWidget {
+//   // the page where user's extracted info is presented
+//   // after processing done by the api-> call this class and pass the arguments that we need !
+//   late String name, org;
+//   UserPage({super.key, required this.name, required this.org});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("User View!"),
+//         centerTitle: true,
+//       ),
+//       body: UserTextField(label:name,showText:""),
+//       // body: Padding(
+//       //   padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+//       //   child: Container(
+//       //     width: double.infinity,
+//       //     height: double.infinity,
+//       //     color: Colors.grey,
+//       //     child: Column(
+//       //       crossAxisAlignment: CrossAxisAlignment.stretch,
+//       //       children: [
+//       //         const Padding(
+//       //           padding: EdgeInsets.fromLTRB(1.0, 10.0, 1.0, 0),
+//       //           child: Text(
+//       //             "Name",
+//       //             style: TextStyle(
+//       //               color: Colors.white,
+//       //               fontSize: 25.0,
+//       //             ),
+//       //           ),
+//       //         ),
+//       //         const SizedBox(
+//       //           height: 8.0,
+//       //         ),
+//       //         Padding(
+//       //           padding: const EdgeInsets.fromLTRB(5, 3, 0, 1),
+//       //           child: Container(
+//       //             height: 50,
+//       //             width: double.maxFinite,
+//       //             decoration: BoxDecoration(
+//       //               color: Colors.white,
+//       //               borderRadius: BorderRadius.circular(5),
+//       //             ),
+//       //             child: Text(
+//       //               name,
+//       //               style: const TextStyle(
+//       //                 color: Colors.black,
+//       //                 fontSize: 25.0,
+//       //               ),
+//       //             ),
+//       //           ),
+//       //         ),
+      
+List<String> predefinedOrg = [
+    'HackerRank',
+    'Udemy',
+    'Coursera',
+    'Linked in',
+    'edX',
+    'Amazon',
+    'Google',
+    'Harvard University',
+    'Simplilearn',
+    'Cisco',
+    'Model Institute of Engineering & Technolos',
+    'ThinkNEXT Technologies Pvt. Ltd.'
+  ];
+
+  List<String> Names = [
+    'Chetan Gupta',
+    'Abhay Sharma' ,
+    'Babita Singh Chib', 
+    'Ankur Gupta',
+    'Raghav gupta',
+    'Aakanksha Kapoor',
+    'Chetan',
+    'Abhay' ,
+    'Ankur',
+    'Aakanksha',
+  ];
+
+//               // const SizedBox(
+//               //   height: 10.0,
+//               // ),
+//               // const Padding(
+//               //   padding: EdgeInsets.fromLTRB(1.0, 10.0, 1.0, 0),
+//               //   child: Text(
+//               //     "Organization",
+//               //     style: TextStyle(
+//               //       color: Colors.white,
+//               //       fontSize: 25.0,
+//               //     ),
+//               //   ),
+//               // ),
+//               // const SizedBox(
+//               //   height: 8.0,
+//               // ),
+//               // Container(
+//               //   height: 60,
+//               //   width: double.maxFinite,
+//               //   decoration: BoxDecoration(
+//               //     color: Colors.white,
+//               //     borderRadius: BorderRadius.circular(5),
+//               //   ),
+//               //   child: Text(
+//               //     org,
+//               //     style: const TextStyle(
+//               //       color: Colors.black,
+//               //       fontSize: 25.0,
+//               //     ),
+//               //   ),
+//               // ),
+//               // const SizedBox(
+//               //   height: 10.0,
+//               // ),
+//               // const Padding(
+//               //   padding: EdgeInsets.fromLTRB(1.0, 10.0, 1.0, 0),
+//               //   child: Text(
+//               //     "Course",
+//               //     style: TextStyle(
+//               //       color: Colors.white,
+//               //       fontSize: 25.0,
+//               //     ),
+//               //   ),
+//               // ),
+//               // const SizedBox(
+//               //   height: 8.0,
+//               // ),
+//               // Container(
+//               //   height: 40,
+//               //   width: double.maxFinite,
+//               //   decoration: BoxDecoration(
+//               //     color: Colors.white,
+//               //     borderRadius: BorderRadius.circular(5),
+//               //   ),
+//               //   child: Text(
+//               //     course,
+//               //     style: const TextStyle(
+//               //       color: Colors.blueAccent,
+//               //       fontSize: 25.0,
+//               //     ),
+//               //   ),
+//               // ),
+//               // const SizedBox(
+//               //   height: 10.0,
+//               // ),
+//     //         ],
+//     //       ),
+//     //     ),
+//     //   ),
+//     // );
+//     );
+//   }
+// }
